e3ae87311c895b651f6d266f93932c6f41d59a78
Geo Joy <me.geojoy@gmail.com>
2016-10-24 18:56:24 +0530

    update 3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "deb http://repo.mongodb.org/apt/debian/ "$(lsb_release -sc)"/mongodb-org/3.3 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.3.list
+$ echo "deb http://repo.mongodb.org/apt/debian "$(lsb_release -sc)"/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 sudo apt-get update
 
