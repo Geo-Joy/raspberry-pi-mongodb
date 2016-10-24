7be656065b792bf00fdbcba43a21ac107261d945
Geo Joy <me.geojoy@gmail.com>
2016-10-24 17:36:56 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "deb http://repo.mongodb.org/apt/debian/dists/"$(lsb_release -sc)"/mongodb-org/3.3 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.3.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+systemctl start mongod
+
+systemctl enable mongod
